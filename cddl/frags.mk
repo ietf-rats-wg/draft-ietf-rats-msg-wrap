ab7deeca6fa5a3058a040c9a5654b09553ee3a52
Thomas Fossati <thomas.fossati@linaro.org>
2024-02-05 18:21:27 +0100

    hook the new cddl builder into the top-level Makefile
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- cddl/frags.mk
+++ cddl/frags.mk
@@ -5,7 +5,11 @@ CMW_FRAGS += cmw-cbor-tag.cddl
 CMW_FRAGS += rfc9193.cddl
 CMW_FRAGS += jc.cddl
 
-CMW_EXAMPLES := $(wildcard cmw-example-*.diag)
+CMW_EXAMPLES := cmw-example-cbor-1.diag
+CMW_EXAMPLES += cmw-example-cbor-2.diag
+CMW_EXAMPLES += cmw-example-cbor-3.diag
+CMW_EXAMPLES += cmw-example-cbor-tag-1.diag
+CMW_EXAMPLES += cmw-example-json-1.diag
 
 COLLECTION_FRAGS := cmw-collection-start.cddl
 COLLECTION_FRAGS += cmw-collection.cddl
@@ -15,4 +19,5 @@ COLLECTION_FRAGS += cmw-cbor-tag.cddl
 COLLECTION_FRAGS += rfc9193.cddl
 COLLECTION_FRAGS += jc.cddl
 
-COLLECTION_EXAMPLES := $(wildcard collection-example-*.diag)
+COLLECTION_EXAMPLES := collection-example-cbor-1.diag
+COLLECTION_EXAMPLES += collection-example-json-1.diag
