a9fe697a34147b7566c2899b99a832272e5dd7b4
Thomas Fossati <thomas.fossati@linaro.org>
2025-01-30 08:53:04 +0100

    drop tunnels (#165)
    Fix #163
    
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- cddl/frags.mk
+++ cddl/frags.mk
@@ -2,7 +2,6 @@ CMW_FRAGS := cmw-start.cddl
 CMW_FRAGS += cmw-record.cddl
 CMW_FRAGS += cmw-cbor-tag.cddl
 CMW_FRAGS += cmw-collection.cddl
-CMW_FRAGS += cmw-tunnel.cddl
 CMW_FRAGS += common-types.cddl
 CMW_FRAGS += rfc9193.cddl
 # CBOR tags used in examples
@@ -25,7 +24,6 @@ EAT_FRAGS := eat-start.cddl
 EAT_FRAGS += cmw-start.cddl
 EAT_FRAGS += cmw-record.cddl
 EAT_FRAGS += cmw-collection.cddl
-EAT_FRAGS += cmw-tunnel.cddl
 EAT_FRAGS += common-types.cddl
 EAT_FRAGS += rfc9193.cddl
 
@@ -35,6 +33,5 @@ COLLECTED_CDDL_FRAGS := cmw-start.cddl
 COLLECTED_CDDL_FRAGS += cmw-record.cddl
 COLLECTED_CDDL_FRAGS += cmw-cbor-tag.cddl
 COLLECTED_CDDL_FRAGS += cmw-collection.cddl
-COLLECTED_CDDL_FRAGS += cmw-tunnel.cddl
 COLLECTED_CDDL_FRAGS += common-types.cddl
 COLLECTED_CDDL_FRAGS += rfc9193.cddl
